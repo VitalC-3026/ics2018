5b90c8685fd86500c9ecc53077faf8be1a02849c
tracer-ics2017 <tracer@njuics.org>
2021-03-17 20:26:31 -0700

    >  compile 1410576 junyun Linux ubuntu 5.4.0-67-generic #75~18.04.1-Ubuntu SMP Tue Feb 23 19:17:50 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:26:31 up  2:54,  1 user,  load average: 0.75, 0.41, 0.29 774037f8ea74bed28a6269bf9f702e722360b4f7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -172,16 +172,17 @@ static bool make_token(char *e) {
 
 // TODO: distinguish (2*3))+4 false / (2+3)*(5-9) false => can be computed
 bool check_parentheses(int p, int q) {
-  printf("p:%d, q:%d, tokens:%d\n", p, q, nr_token);
-  printf("tokens[%d].type: %d\n", p, tokens[p].type);
-  printf("tokens[%d].type: %d\n", q, tokens[q].type);
+  // printf("p:%d, q:%d, tokens:%d\n", p, q, nr_token);
+  // printf("tokens[%d].type: %d\n", p, tokens[p].type);
+  // printf("tokens[%d].type: %d\n", q, tokens[q].type);
   if (tokens[p].type != TK_LP || tokens[q].type != TK_RP) {
+    printf("check_parentheses failed\n");
     return false;
   }
   int t = p + 1;
   int count = 0;
   while(t < q) {
-    printf("token_location: %d, token_value: %s.\n", t, tokens[t].str);
+    // printf("token_location: %d, token_value: %s.\n", t, tokens[t].str);
     if (tokens[t].type == TK_LP) {
       count++;
     }
