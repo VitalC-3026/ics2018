18ab98d68695f4bc45e9a223b4d33d63c554e847
tracer-ics2017 <tracer@njuics.org>
2021-03-26 19:51:55 -0700

    >  compile 1410576 junyun Linux ubuntu 5.4.0-70-generic #78~18.04.1-Ubuntu SMP Sat Mar 20 14:10:07 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:51:55 up 46 min,  1 user,  load average: 1.16, 0.56, 0.30 418dea90a5d959c8c9fbd69adf636eb71d2d41e8

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -50,6 +50,8 @@ static int cmd_w(char *args); // set watchpoints
 
 static int cmd_d(char *args); // delete watchpoints
 
+static int cmd_t(char *args);
+
 static struct {
   char *name;
   char *description;
