d41f29385dab16e05526c1745db64bd2ea243f41
tracer-ics2017 <tracer@njuics.org>
2021-04-24 03:16:38 -0700

    >  compile 1410576 junyun Linux ubuntu 5.4.0-72-generic #80~18.04.1-Ubuntu SMP Mon Apr 12 23:26:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:16:38 up  4:48,  1 user,  load average: 0.09, 0.15, 0.25 f77490c2e24f7c68ce611e637e755c0dc0a894d8

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,16 +14,143 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+    case CC_O: {
+      rtl_get_OF(&t0);
+      if (t0 == 1){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_NO: {
+      rtl_get_OF(&t0);
+      if (t0 == 0){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_B: {
+      rtl_get_CF(&t0);
+      if (t0 == 1){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_NB: {
+      rtl_get_CF(&t0);
+      if (t0 == 0){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_E: {
+      rtl_get_ZF(&t0);
+      if (t0 == 1){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_NE: {
+      rtl_get_ZF(&t0);
+      if (t0 == 0){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_BE: {
+      rtl_get_CF(&t0);
+      rtl_get_ZF(&t1);
+      if (t0 == 1 || t1 == 1){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_NBE: {
+      rtl_get_CF(&t0);
+      rtl_get_ZF(&t1);
+      if (t0 == 0 && t1 == 0){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_S: {
+      rtl_get_SF(&t0);
+      if (t0 == 1){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_NS: {
+      rtl_get_SF(&t0);
+      if (t0 == 0){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_L: {
+      rtl_get_OF(&t0);
+      rtl_get_SF(&t1);
+      if (t0 != t1){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_NL: {
+      rtl_get_OF(&t0);
+      rtl_get_SF(&t1);
+      if (t0 == t1){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_LE: {
+      rtl_get_OF(&t0);
+      rtl_get_SF(&t1);
+      rtl_get_ZF(&t2);
+      if (t0 != t1 || t2 == 1){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
+    case CC_NLE: {
+      rtl_get_OF(&t0);
+      rtl_get_SF(&t1);
+      rtl_get_ZF(&t2);
+      if (t0 == t1 && t2 != 1){
+        *dest = 1;
+      } else {
+        *dest = 0;
+      }
+      break;
+    }
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
+    case CC_NP: panic("n86 does not have PF");
   }
 
   if (invert) {
