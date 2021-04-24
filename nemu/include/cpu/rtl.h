0124184cb939ba9a3d6bc46725e9b69230e93e60
tracer-ics2017 <tracer@njuics.org>
2021-04-23 18:32:00 -0700

    >  compile 1410576 junyun Linux ubuntu 5.4.0-72-generic #80~18.04.1-Ubuntu SMP Mon Apr 12 23:26:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:32:00 up 30 min,  1 user,  load average: 0.54, 0.61, 0.78 99b1fd2f407b9b6a37ebd506f4e65b0e4f3377da

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -155,7 +155,9 @@ static inline void rtl_push(const rtlreg_t* src1) {
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  TODO();
+  rtl_sm(dest, 4, &cpu.esp);
+  rtl_addi(&cpu.esp, &cpu.esp, 4);
+  // TODO();
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
