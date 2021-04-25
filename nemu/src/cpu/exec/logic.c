baab9d509c206e48b75f65d10d818c6153e361f6
tracer-ics2017 <tracer@njuics.org>
2021-04-25 05:54:43 -0700

    >  compile 1410576 junyun Linux ubuntu 5.4.0-72-generic #80~18.04.1-Ubuntu SMP Mon Apr 12 23:26:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  05:54:43 up  5:42,  1 user,  load average: 1.21, 0.42, 0.29 a1e1c0744c973c1dc50c7a65733ab4c73d1ca84f

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -58,7 +58,7 @@ make_EHelper(shl) {
 }
 
 make_EHelper(shr) {
-  TODO();
+  // TODO();
   // unnecessary to update CF and OF in NEMU
   rtl_shr(&t0, &id_dest->val, &id_src->val);
   rtl_update_ZFSF(&t0, id_dest->width);
