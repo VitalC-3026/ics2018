a7aea0a18f20c4fd88c389d5bdaff713e3d592fe
tracer-ics2017 <tracer@njuics.org>
2021-04-26 06:56:03 -0700

    >  compile 1410576 junyun Linux ubuntu 5.4.0-72-generic #80~18.04.1-Ubuntu SMP Mon Apr 12 23:26:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:56:03 up  6:30,  1 user,  load average: 0.35, 0.28, 0.31 c3de7c3a1f32196ea9016b515f488b1487a3f582

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -56,8 +56,14 @@ make_EHelper(dec) {
 }
 
 make_EHelper(neg) {
-  TODO();
-
+  // TODO();
+  t0 = id_dest->val == 0 ? 0 : 1;
+  rtl_set_CF(&t0);
+  rtl_sub(&t1, &tzero, &id_dest->val);
+  operand_write(id_dest, &t1);
+  rtl_update_ZFSF(&t1, id_dest->width);
+  rtl_is_sub_overthrow(&t2, &t1, &tzero, &id_dest->val, id_dest->width);
+  rtl_set_OF(&t2);
   print_asm_template1(neg);
 }
 
