9d88d98a27db2d1b423abea0ccc2c817784617f9
tracer-ics2017 <tracer@njuics.org>
2021-04-28 11:07:34 -0700

    >  compile 1410576 junyun Linux ubuntu 5.4.0-72-generic #80~18.04.1-Ubuntu SMP Mon Apr 12 23:26:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:07:34 up  1:56,  1 user,  load average: 0.80, 0.81, 0.73 b32f0eb096c4d300a3a5451494ae56e9592940ca

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -184,6 +184,9 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
   // TODO();
   rtl_shri(dest, src1, width * 8 - 1);
+  // TODO-ADD
+  rtl_andi(dest, dest, 0x1);
+
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
