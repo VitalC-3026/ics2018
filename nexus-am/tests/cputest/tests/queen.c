074847c6e1580a2c7e61041a1c7896f6c92d3cea
tracer-ics2017 <tracer@njuics.org>
2021-04-28 06:14:20 -0700

    >  run 1410576 junyun Linux ubuntu 5.4.0-72-generic #80~18.04.1-Ubuntu SMP Mon Apr 12 23:26:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:14:20 up 38 min,  1 user,  load average: 0.65, 0.39, 0.27 457a56822d04301c8ca5622d6827c57a604fd5e1

---
+++ nexus-am/tests/cputest/tests/queen.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "trap.h"
+
+static unsigned int FULL;
+
+static unsigned int dfs(unsigned int row, unsigned int ld, unsigned int rd) {
+  if (row == FULL) {
+    return 1;
+  } else {
+    unsigned int pos = FULL & (~(row | ld | rd)), ans = 0;
+    while (pos) {
+      unsigned int p = (pos & (~pos + 1));
+      pos -= p;
+      ans += dfs(row | p, (ld | p) << 1, (rd | p) >> 1);
+    }
+    return ans;
+  }
+}
+
+static unsigned int ans;
+
+// void bench_queen_prepare() {
+//   ans = 0;
+//   FULL = (1 << setting->size) - 1;
+// }
+
+// void bench_queen_run() {
+//   ans = dfs(0, 0, 0);
+// }
+
+// int bench_queen_validate() {
+//   return ans == setting->checksum;
+// }
+
+int main() {
+    int res[] = {92, 14200};
+    int size[] = {8, 12};
+    for (int i = 0; i < 2; i++) {
+        FULL = (1 << size[i]) - 1;
+        ans = 0;
+        ans = dfs(0, 0, 0);
+        nemu_assert(ans != res[i]);
+    }
+} 
