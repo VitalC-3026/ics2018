58e35f50aa339319b6af62e69c51faab8bfe5a75
tracer-ics2017 <tracer@njuics.org>
2021-04-29 23:53:36 -0700

    >  compile 1410576 junyun Linux ubuntu 5.4.0-72-generic #80~18.04.1-Ubuntu SMP Mon Apr 12 23:26:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:53:36 up  1:13,  1 user,  load average: 0.86, 0.47, 0.62 3043699fcf03f78fac8cf297f1b7a50111739908

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -210,19 +210,19 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
       assert(0);
   }
   if ((*result & mask) == 0) {
-    t3 = 1;
+    s0 = 1;
   }
   else {
-    t3 = 0;
+    s0 = 0;
   }
-  rtl_set_ZF(&t3);
+  rtl_set_ZF(&s0);
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
   // TODO();
-  rtl_msb(&t3, result, width);
-  rtl_set_SF(&t3);
+  rtl_msb(&s0, result, width);
+  rtl_set_SF(&s0);
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
