d1238e9cffb6c912b7319d748287ac3d6c3df9d1
tracer-ics2017 <tracer@njuics.org>
2021-05-12 19:49:30 -0700

    >  run 1811499 junyun Linux ubuntu 5.4.0-72-generic #80~18.04.1-Ubuntu SMP Mon Apr 12 23:26:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:49:30 up  1:44,  1 user,  load average: 0.77, 0.72, 0.56 34e22f04e55db7bce0bdcc34e7a810aa70e103b9

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,14 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+void ramdisk_read(void* buf, off_t offset, size_t len);
+void ramdisk_write(const void* buf, off_t offset, size_t len);
+size_t get_ramdisk_size();
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //TODO();
+  // raw program loader
+  size_t len = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY, 0, len);
   return (uintptr_t)DEFAULT_ENTRY;
 }
