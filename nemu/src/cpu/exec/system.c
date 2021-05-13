ce6588c0adaac894c67381f37d94e3faa654913b
tracer-ics2017 <tracer@njuics.org>
2021-05-12 20:18:03 -0700

    >  compile 1811499 junyun Linux ubuntu 5.4.0-72-generic #80~18.04.1-Ubuntu SMP Mon Apr 12 23:26:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:18:03 up  2:13,  1 user,  load average: 0.53, 0.33, 0.36 bcb0a6622468bde2f72da14139778a9fda6d9526

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -24,9 +24,11 @@ make_EHelper(mov_cr2r) {
 #endif
 }
 
-make_EHelper(int) {
-  TODO();
+void raise_intr(uint8_t NO, vaddr_t ret_addr);
 
+make_EHelper(int) {
+  //TODO();
+  raise_intr(id_dest->val, decoding.seq_eip);
   print_asm("int %s", id_dest->str);
 
 #ifdef DIFF_TEST
