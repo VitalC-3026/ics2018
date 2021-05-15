4e7203277e095f0a4c680b0a457cd8a0b1a29c23
tracer-ics2017 <tracer@njuics.org>
2021-05-15 03:33:07 -0700

    >  compile 1811499 junyun Linux ubuntu 5.4.0-73-generic #82~18.04.1-Ubuntu SMP Fri Apr 16 15:10:02 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:33:07 up  4:05,  1 user,  load average: 0.19, 0.24, 0.10 cb0e4507c5996f0e7cfde0b53dbc73f3c924d34

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -51,7 +51,7 @@ make_EHelper(iret) {
   decoding.is_jmp = 1;
   rtl_pop(&cpu.cs);
   rtl_pop(&cpu.eflags.val);
-  //decoding.seq_eip = cpu.eip; // iret QA
+  decoding.seq_eip = cpu.eip; // iret QA
   print_asm("iret");
 }
 
