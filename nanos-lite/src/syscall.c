c6b4b242579b39f1f9b916f4ddaffce1696abd34
tracer-ics2017 <tracer@njuics.org>
2021-05-15 03:38:26 -0700

    >  run 1811499 junyun Linux ubuntu 5.4.0-73-generic #82~18.04.1-Ubuntu SMP Fri Apr 16 15:10:02 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:38:26 up  4:11,  1 user,  load average: 1.46, 0.87, 0.38 5cb0c442e703b06bfe22fd8b6b68b323bfab42a8

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -21,7 +21,6 @@ static inline _RegSet* sys_write(_RegSet *r) {
       _putc(buf[i]);
     }
     SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
-    
   }
   else {
     panic("Unhandled fd=%d in sys_write.\n", fd);
@@ -37,7 +36,7 @@ _RegSet* do_syscall(_RegSet *r) {
   switch (a[0]) {
     case SYS_none: return sys_none(r);
     case SYS_exit: return sys_exit(r);
-    case SYS_write: return 0;
+    case SYS_write: return sys_write(r);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
