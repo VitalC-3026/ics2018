010d9cc97c9cf850e33c13d7d85f49b3cbd9e420
tracer-ics2017 <tracer@njuics.org>
2021-05-19 19:46:59 -0700

    >  run 1811499 junyun Linux ubuntu 5.4.0-73-generic #82~18.04.1-Ubuntu SMP Fri Apr 16 15:10:02 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:46:59 up  1:41,  1 user,  load average: 0.16, 0.27, 0.18 ea0b34249d98dd2e09851d6cbdfa19629598ed5

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -35,9 +35,11 @@ static intptr_t brk = (intptr_t)&_end;
 void *_sbrk(intptr_t increment){
   intptr_t old_brk = brk;
   intptr_t new_brk = old_brk + increment;
+  char buf1[10];
+    sprintf(buf1, "sbrk1");
   if (_syscall_(SYS_brk, new_brk, 0, 0) == 0) {
-    char buf[10];
-    sprintf(buf, "sbrk");
+    char buf2[10];
+    sprintf(buf2, "sbrk2");
     brk = new_brk;
     return (void *)old_brk;
   }
