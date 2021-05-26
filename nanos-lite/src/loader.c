2a11d58937f5a31eb5cd7a764d0d5129a58572c4
tracer-ics2017 <tracer@njuics.org>
2021-05-26 00:25:23 -0700

    >  run 1811499 junyun Linux ubuntu 5.4.0-73-generic #82~18.04.1-Ubuntu SMP Fri Apr 16 15:10:02 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:25:23 up 1 min,  1 user,  load average: 3.54, 1.44, 0.53 edf27ccedf0dd8b17ce1d8539ea9d11b95c4f453

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,10 +6,23 @@ void ramdisk_read(void* buf, off_t offset, size_t len);
 void ramdisk_write(const void* buf, off_t offset, size_t len);
 size_t get_ramdisk_size();
 
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_filesz(int fd);
+size_t fs_read(int fd, void *buf, size_t len);
+int fs_close(int fd);
+
+const char filename[] = "/bin/text";
+
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
   // raw program loader
-  size_t len = get_ramdisk_size();
-  ramdisk_read(DEFAULT_ENTRY, 0, len);
+  // size_t len = get_ramdisk_size();
+  // ramdisk_read(DEFAULT_ENTRY, 0, len);
+  // return (uintptr_t)DEFAULT_ENTRY;
+  // raw filesystem loader
+  int fd = fs_open(filename, 0, 0);
+  size_t size = fs_filesz(fd);
+  fs_read(fd, DEFAULT_ENTRY, size);
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
