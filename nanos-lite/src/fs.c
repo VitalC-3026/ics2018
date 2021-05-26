ddf6c667c52081242f48c4f9b0a6252c4354025b
tracer-ics2017 <tracer@njuics.org>
2021-05-26 00:50:21 -0700

    >  run 1811499 junyun Linux ubuntu 5.4.0-73-generic #82~18.04.1-Ubuntu SMP Fri Apr 16 15:10:02 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:50:21 up 26 min,  1 user,  load average: 0.37, 0.46, 0.49 22a7c9598426aa6f06613b5884cc3f2fc5162e

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -77,6 +77,8 @@ size_t fs_write(int fd, const void* buf, size_t len) {
   if(fs_filesz(fd) < len + file_table[fd].open_offset) {
     len = fs_filesz(fd) - file_table[fd].open_offset;
   }
+  // TODO-ADD
+  int offset = file_table[fd].open_offset + file_table[fd].disk_offset;
   switch(fd) {
     case FD_STDIN:
       return 0;
@@ -93,11 +95,11 @@ size_t fs_write(int fd, const void* buf, size_t len) {
       break;
     }
     default: {
-      ramdisk_write(buf, file_table[fd].open_offset + file_table[fd].disk_offset, len);
+      ramdisk_write(buf, offset, len);
       file_table[fd].open_offset += len;
     }
   }
-  //Log("Write: file %s, open_off %d, disk_off %d, len %d.\n", file_table[fd].name, file_table[fd].open_offset, file_table[fd].disk_offset, len);
+  Log("Write: file %s, open_off %d, disk_off %d, len %d.\n", file_table[fd].name, file_table[fd].open_offset, file_table[fd].disk_offset, len);
   return len;
 }
 
