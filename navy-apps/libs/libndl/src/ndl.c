59cb77c2ef1b6819ad4a1eb156105aae8c8a7e83
tracer-ics2017 <tracer@njuics.org>
2021-05-26 19:20:16 -0700

    >  run 1811499 junyun Linux ubuntu 5.4.0-73-generic #82~18.04.1-Ubuntu SMP Fri Apr 16 15:10:02 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:20:16 up  1:26,  1 user,  load average: 0.34, 0.18, 0.14 aca4357aa49100cf3c5a4775e21b8322598a4e15

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -134,6 +134,7 @@ static void get_display_info() {
     *(delim = strchr(buf, ':')) = '\0';
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
+    Log("key: %s.\tvalue: %s.", key, value);
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }
