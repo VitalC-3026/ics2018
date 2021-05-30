8a84c0ad0627375cc2b4445babb71c9f91cb6494
tracer-ics2017 <tracer@njuics.org>
2021-05-29 18:50:35 -0700

    >  compile 1811499 junyun Linux ubuntu 5.4.0-73-generic #82~18.04.1-Ubuntu SMP Fri Apr 16 15:10:02 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:50:35 up 48 min,  1 user,  load average: 1.60, 1.19, 0.68 35da3bbc129ad231e654f882824544bdb940980b

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -29,7 +29,7 @@ int main() {
 #endif
 
   init_fs();
-  const char filename[] = "/bin/pal";
+  const char filename[] = "/bin/dummy";
   uint32_t entry = loader(NULL, filename);
   ((void (*)(void))entry)();
 
