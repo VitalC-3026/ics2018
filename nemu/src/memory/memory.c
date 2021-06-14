674c305087a856b89edfb5edbcae409442cc9593
tracer-ics2017 <tracer@njuics.org>
2021-06-13 23:08:57 -0700

    >  compile 1811499 junyun Linux ubuntu 5.4.0-74-generic #83~18.04.1-Ubuntu SMP Tue May 11 16:01:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:08:57 up  4:47,  1 user,  load average: 1.34, 0.62, 0.23 b4cce5d719c980d94483856aaa9fd1e59bbc26c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -74,7 +74,8 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  if (((addr+len-1) & ~0xfff) != (addr & ~0xfff)) {
+  if (false) {
+    // ((addr+len-1) & ~0xfff) != (addr & ~0xfff)
     printf("va 0x%x\n", addr);
     printf("eip: 0x%x\n", cpu.eip);
     assert(0);
@@ -86,7 +87,8 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  if (((addr+len-1) & ~0xfff) != (addr & ~0xfff)) {
+  if (false) {
+    // ((addr+len-1) & ~0xfff) != (addr & ~0xfff)
     assert(0);
   }
   else {
