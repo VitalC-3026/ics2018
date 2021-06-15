e2c660b73381ea6ceccc328cb5c483ad2c20861f
tracer-ics2017 <tracer@njuics.org>
2021-06-15 15:32:37 +0800

    >  run 1811499 junyun Linux junyun-virtual-machine 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:19 UTC 2019 i686 i686 i686 GNU/Linux  15:32:37 up 24 min,  1 user,  load average: 0.34, 0.47, 0.46 83e32c36429890e4de85446e94a9d2cc5da3c3e

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,7 +8,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL: return do_syscall(r);
     case _EVENT_TRAP: return schedule(r);
-    case _EVENT_IRQ_TIME: Log("IRQ_TIME"); return schedule(r);
+    case _EVENT_IRQ_TIME: return schedule(r); //Log("IRQ_TIME");
     default: panic("Unhandled event ID = %d", e.event);
   }
 
